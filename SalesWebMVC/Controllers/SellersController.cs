2ff7fe8b83f3d56b766db1b3214664a55d3c0888
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-27 15:19:52 -0300

    Implementação do do serviço de vendedores (Sellerservice) e o método FindAll()

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using SalesWebMVC.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        public readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();
+            return View(list);
         }
     }
 }
--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 using SalesWebMVC.Data;
-
+using SalesWebMVC.Services;
 namespace SalesWebMVC
 {
     public class Startup
@@ -42,6 +42,7 @@ namespace SalesWebMVC
                         builder.MigrationsAssembly("SalesWebMVC")));
             //Registrar o serviço na injeção de dependência da aplicação.
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
