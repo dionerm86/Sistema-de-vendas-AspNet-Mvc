2490652edc471059768200af17785cd8ef91afae
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-27 16:13:55 -0300

    Implementado o métod InsertSellers para cadastrar vendedores.

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebMVC.Services;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Controllers
 {
@@ -16,6 +17,22 @@ namespace SalesWebMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Indicar que a ação será um post e não um get
+        [HttpPost]
+        //Previnir que a aplicação sofra ataque CSRF (dados maliciosos)
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.InsertSeller(seller);
+            // redirecionar a requisição para a ação Index, que é ação que mostra a tela principal do crud de vendedores.
+            return RedirectToAction(nameof(Index));
         }
     }
 }
